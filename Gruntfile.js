bbdc9754008cbd2646e3e0f95fa44dd3bb031827
Arve Knudsen <arve.knudsen@gmail.com>
2013-12-27 01:50:26 +0100

    Add example which includes pre-built library

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,7 @@
 module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-coffee');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     grunt.initConfig({
         coffee: {
@@ -9,8 +11,43 @@ module.exports = function (grunt) {
                     'lib/chuck/helpers.js': 'src/chuck/helpers.coffee',
                     'lib/chuck/lexer.js': 'src/chuck/lexer.coffee',
                     'lib/chuck.js': 'src/chuck.coffee',
-                    'testparse.js': 'src/testparse.coffee'
+                    'lib/chuck/audioContextService.js': 'src/chuck/audioContextService.coffee',
+                    'lib/chuck/instructions.js': 'src/chuck/instructions.coffee',
+                    'lib/chuck/logging.js': 'src/chuck/logging.coffee',
+                    'lib/chuck/nodes.js': 'src/chuck/nodes.coffee',
+                    'lib/chuck/parserService.js': 'src/chuck/parserService.coffee',
+                    'lib/chuck/scanner.js': 'src/chuck/scanner.coffee',
+                    'lib/chuck/types.js': 'src/chuck/types.coffee',
+                    'lib/chuck/ugen.js': 'src/chuck/ugen.coffee',
+                    'lib/chuck/vm.js': 'src/chuck/vm.coffee'
                 }
+            }
+        },
+        copy: {
+            npm: {
+                files: [
+                    {
+                        expand: true, cwd: 'node_modules', flatten: true, dest: 'lib/', filter: 'isFile',
+                        src: [
+                            'underscore/underscore.js',
+                            'underscore.string/lib/underscore.string.js',
+                            'almond/almond.js'
+                        ]
+                    }
+                ]
+            },
+            lib: {
+                files: [
+                    {expand: true, cwd: 'src/lib/', src: ['q.js'], dest: 'lib/'}
+                ]
+            }
+        },
+        shell: {
+            minify: {
+                options: { failOnError: true, stdout: true, stderr: true },
+                command: 'node node_modules/requirejs/bin/r.js -o baseUrl=lib name=almond include=' +
+                    'chuck,underscore,underscore.string,chuck/parser wrap=false optimize=none ' +
+                    'out=examples/js/chuck.js'
             }
         }
     });
@@ -20,8 +57,8 @@ module.exports = function (grunt) {
         var parserGenerator = require('./lib/grammar').generate;
         var parserCode = parserGenerator();
         parserCode = parserCode + "\nwindow.ChuckParser = parser.Parser;\n";
-        grunt.file.write('lib/parser.js', parserCode);
+        grunt.file.write('lib/chuck/parser.js', parserCode);
     });
 
-    grunt.registerTask('default', ['coffee', 'parser']);
+    grunt.registerTask('default', ['coffee', 'parser', 'copy', 'shell']);
 };
