323a49ba34635a47deecbf778acbf33ac8f6e619
Arve Knudsen <arve.knudsen@gmail.com>
2014-03-16 15:07:30 +0100

    Indentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,120 +1,120 @@
 module.exports = function (grunt) {
-    grunt.loadNpmTasks('grunt-contrib-coffee');
-    grunt.loadNpmTasks('grunt-shell');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-stencil')
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-stencil')
 
-    grunt.initConfig({
-        coffee: {
-            compile: {
-                files: {
-                    'lib/grammar.js': 'language/grammar.coffee',
-                    'lib/chuck/helpers.js': 'src/chuck/helpers.coffee',
-                    'lib/chuck/lexer.js': 'src/chuck/lexer.coffee',
-                    'lib/chuck.js': 'src/chuck.coffee',
-                    'lib/chuck/audioContextService.js': 'src/chuck/audioContextService.coffee',
-                    'lib/chuck/instructions.js': 'src/chuck/instructions.coffee',
-                    'lib/chuck/logging.js': 'src/chuck/logging.coffee',
-                    'lib/chuck/nodes.js': 'src/chuck/nodes.coffee',
-                    'lib/chuck/parserService.js': 'src/chuck/parserService.coffee',
-                    'lib/chuck/scanner.js': 'src/chuck/scanner.coffee',
-                    'lib/chuck/types.js': 'src/chuck/types.coffee',
-                    'lib/chuck/ugen.js': 'src/chuck/ugen.coffee',
-                    'lib/chuck/vm.js': 'src/chuck/vm.coffee',
-                    'lib/chuck/namespace.js': 'src/chuck/namespace.coffee',
-                    'lib/chuck/libs/math.js': 'src/chuck/libs/math.coffee'
-                }
-            },
-            compileExamples: {
-                options: { bare: true },
-                expand: true,
-                flatten: false,
-                cwd: 'pages/',
-                src: ['**/*.coffee'],
-                dest: 'examples/',
-                ext: '.js'
-            }
-        },
-        copy: {
-            npm: {
-                files: [
-                    {
-                        expand: true, cwd: 'node_modules', flatten: true, dest: 'lib/', filter: 'isFile',
-                        src: [
-                            'lodash/lodash.js',
-                            'underscore.string/lib/underscore.string.js',
-                            'almond/almond.js'
-                        ]
-                    }
-                ]
-            },
-            lib: {
-                files: [
-                    {expand: true, cwd: 'src/lib/', src: ['q.js'], dest: 'lib/'}
-                ]
-            },
-            stencil: {
-                files: [
-                    {
-                        expand: true, cwd: 'pages', dest: 'examples/', filter: 'isFile',
-                        src: [
-                            '**/*.js', '**/*.css'
-                        ]
-                    }
-                ]
-            }
-        },
-        shell: {
-            minify: {
-                options: { failOnError: true, stdout: true, stderr: true },
-                command: 'node node_modules/requirejs/bin/r.js -o baseUrl=lib name=almond include=' +
-                    'chuck,lodash,underscore.string,chuck/parser wrap=false optimize=none ' +
-                    'out=examples/js/chuck.js'
-            }
-        },
-        stencil: {
-            options: {
-                templates: 'pages/templates'
-            },
-            main: {
-                options: {
-                    env: {
-                        root: ''
-                    }
-                },
-                files: {
-                    'examples/example1.html': ['pages/example1.dot.html'],
-                    'examples/example2.html': ['pages/example2.dot.html']
-                }
-            },
-            basic: {
-                options: {
-                    env: {
-                        root: '../'
-                    }
-                },
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'pages',
-                        src: 'basic/**/*.dot.html',
-                        dest: 'examples/',
-                        ext: '.html',
-                        filter: 'isFile'
-                    }
-                ]
-            }
-
+  grunt.initConfig({
+    coffee: {
+      compile: {
+        files: {
+          'lib/grammar.js': 'language/grammar.coffee',
+          'lib/chuck/helpers.js': 'src/chuck/helpers.coffee',
+          'lib/chuck/lexer.js': 'src/chuck/lexer.coffee',
+          'lib/chuck.js': 'src/chuck.coffee',
+          'lib/chuck/audioContextService.js': 'src/chuck/audioContextService.coffee',
+          'lib/chuck/instructions.js': 'src/chuck/instructions.coffee',
+          'lib/chuck/logging.js': 'src/chuck/logging.coffee',
+          'lib/chuck/nodes.js': 'src/chuck/nodes.coffee',
+          'lib/chuck/parserService.js': 'src/chuck/parserService.coffee',
+          'lib/chuck/scanner.js': 'src/chuck/scanner.coffee',
+          'lib/chuck/types.js': 'src/chuck/types.coffee',
+          'lib/chuck/ugen.js': 'src/chuck/ugen.coffee',
+          'lib/chuck/vm.js': 'src/chuck/vm.coffee',
+          'lib/chuck/namespace.js': 'src/chuck/namespace.coffee',
+          'lib/chuck/libs/math.js': 'src/chuck/libs/math.coffee'
         }
-    });
+      },
+      compileExamples: {
+        options: { bare: true },
+        expand: true,
+        flatten: false,
+        cwd: 'pages/',
+        src: ['**/*.coffee'],
+        dest: 'examples/',
+        ext: '.js'
+      }
+    },
+    copy: {
+      npm: {
+        files: [
+          {
+            expand: true, cwd: 'node_modules', flatten: true, dest: 'lib/', filter: 'isFile',
+            src: [
+              'lodash/lodash.js',
+              'underscore.string/lib/underscore.string.js',
+              'almond/almond.js'
+            ]
+          }
+        ]
+      },
+      lib: {
+        files: [
+          {expand: true, cwd: 'src/lib/', src: ['q.js'], dest: 'lib/'}
+        ]
+      },
+      stencil: {
+        files: [
+          {
+            expand: true, cwd: 'pages', dest: 'examples/', filter: 'isFile',
+            src: [
+              '**/*.js', '**/*.css'
+            ]
+          }
+        ]
+      }
+    },
+    shell: {
+      minify: {
+        options: { failOnError: true, stdout: true, stderr: true },
+        command: 'node node_modules/requirejs/bin/r.js -o baseUrl=lib name=almond include=' +
+          'chuck,lodash,underscore.string,chuck/parser wrap=false optimize=none ' +
+          'out=examples/js/chuck.js'
+      }
+    },
+    stencil: {
+      options: {
+        templates: 'pages/templates'
+      },
+      main: {
+        options: {
+          env: {
+            root: ''
+          }
+        },
+        files: {
+          'examples/example1.html': ['pages/example1.dot.html'],
+          'examples/example2.html': ['pages/example2.dot.html']
+        }
+      },
+      basic: {
+        options: {
+          env: {
+            root: '../'
+          }
+        },
+        files: [
+          {
+            expand: true,
+            cwd: 'pages',
+            src: 'basic/**/*.dot.html',
+            dest: 'examples/',
+            ext: '.html',
+            filter: 'isFile'
+          }
+        ]
+      }
 
-    grunt.registerTask('parser', 'Generate parser', function () {
-        grunt.task.requires('coffee');
-        var parserGenerator = require('./lib/grammar').generate;
-        var parserCode = parserGenerator();
-        parserCode = parserCode + "\nwindow.ChuckParser = parser.Parser;\n";
-        grunt.file.write('lib/chuck/parser.js', parserCode);
-    });
+    }
+  });
 
-    grunt.registerTask('default', ['coffee', 'parser', 'copy', 'shell', 'stencil']);
+  grunt.registerTask('parser', 'Generate parser', function () {
+    grunt.task.requires('coffee');
+    var parserGenerator = require('./lib/grammar').generate;
+    var parserCode = parserGenerator();
+    parserCode = parserCode + "\nwindow.ChuckParser = parser.Parser;\n";
+    grunt.file.write('lib/chuck/parser.js', parserCode);
+  });
+
+  grunt.registerTask('default', ['coffee', 'parser', 'copy', 'shell', 'stencil']);
 };
--- karma.conf.js
+++ karma.conf.js
@@ -1,82 +1,80 @@
 // Karma configuration
-// Generated on Tue Dec 10 2013 12:56:53 GMT+0100 (CET)
-
 module.exports = function (config) {
-    config.set({
-        // base path, that will be used to resolve files and exclude
-        basePath: 'src',
+  config.set({
+    // base path, that will be used to resolve files and exclude
+    basePath: 'src',
 
-        // frameworks to use
-        frameworks: ['jasmine'],
+    // frameworks to use
+    frameworks: ['jasmine'],
 
-        // list of files / patterns to load in the browser
-        files: [
-            // Load q before require, since we don't want it to register itself as a module
-            'lib/q.js',
-            '../node_modules/lodash/lodash.js',
-            '../node_modules/underscore.string/lib/underscore.string.js',
-            '../node_modules/requirejs/require.js',
-            '../node_modules/karma-requirejs/lib/adapter.js',
-            'test-main.coffee',
-            {pattern: '**/*.coffee', included: false},
-            {pattern: '**/*.js', included: false},
-            '../lib/chuck/parser.js'
-        ],
+    // list of files / patterns to load in the browser
+    files: [
+      // Load q before require, since we don't want it to register itself as a module
+      'lib/q.js',
+      '../node_modules/lodash/lodash.js',
+      '../node_modules/underscore.string/lib/underscore.string.js',
+      '../node_modules/requirejs/require.js',
+      '../node_modules/karma-requirejs/lib/adapter.js',
+      'test-main.coffee',
+      {pattern: '**/*.coffee', included: false},
+      {pattern: '**/*.js', included: false},
+      '../lib/chuck/parser.js'
+    ],
 
-        // list of files to exclude
-        exclude: [],
+    // list of files to exclude
+    exclude: [],
 
-        // test results reporter to use
-        // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-        reporters: ['progress', 'growler'],
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress', 'growler'],
 
-        // web server port
-        port: 9876,
+    // web server port
+    port: 9876,
 
-        // enable / disable colors in the output (reporters and logs)
-        colors: true,
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
-        // level of logging
-        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_INFO,
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
-        // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: true,
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
 
-        // Start these browsers, currently available:
-        // - Chrome
-        // - ChromeCanary
-        // - Firefox
-        // - Opera (has to be installed with `npm install karma-opera-launcher`)
-        // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
-        // - PhantomJS
-        // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-        browsers: ['Chrome', 'PhantomJS'],
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera (has to be installed with `npm install karma-opera-launcher`)
+    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+    // - PhantomJS
+    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+    browsers: ['Chrome', 'PhantomJS'],
 
-        // If browser does not capture in given timeout [ms], kill it
-        captureTimeout: 60000,
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
 
-        // Continuous Integration mode
-        // if true, it capture browsers, run tests and exit
-        singleRun: false,
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false,
 
-        preprocessors: {
-            '**/*.coffee': ['coffee']
-        },
-        coffeePreprocessor: {
-            options: {
-                bare: true,
-                sourceMap: false
-            }
-        },
+    preprocessors: {
+      '**/*.coffee': ['coffee']
+    },
+    coffeePreprocessor: {
+      options: {
+        bare: true,
+        sourceMap: false
+      }
+    },
 
-        plugins: [
-            'karma-phantomjs-launcher',
-            'karma-jasmine',
-            'karma-chrome-launcher',
-            'karma-growler-reporter',
-            'karma-requirejs',
-            'karma-coffee-preprocessor'
-        ]
-    });
+    plugins: [
+      'karma-phantomjs-launcher',
+      'karma-jasmine',
+      'karma-chrome-launcher',
+      'karma-growler-reporter',
+      'karma-requirejs',
+      'karma-coffee-preprocessor'
+    ]
+  });
 };
